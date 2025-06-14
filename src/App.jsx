b7f90d4484924be54f86bae74845a6d321206365
Subodh-Khadka <subodhkdk1100@gmail.com>
2025-06-14 11:07:44 +0545

    use state to update player name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player 1" symbol="X" />
+          <Player initialName="player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
