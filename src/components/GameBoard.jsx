cd31dd68ef8daea25ceca11c8e12d23eb545c1fa
Subodh-Khadka <subodhkdk1100@gmail.com>
2025-06-21 23:10:39 +0545

    projectCompletion

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,41 +1,15 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedGameBoard[rowIndex][colIndex] = activeplayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
@@ -46,3 +20,17 @@ export default function GameBoard({ onSelectSquare, turns }) {
     </ol>
   );
 }
+
+// const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+// function handleSelectSquare(rowIndex, colIndex) {
+//   setGameBoard((prevGameBoard) => {
+//     const updatedGameBoard = [
+//       ...prevGameBoard.map((innerArray) => [...innerArray]),
+//     ];
+//     updatedGameBoard[rowIndex][colIndex] = activeplayerSymbol;
+//     return updatedGameBoard;
+//   });
+
+//   onSelectSquare();
+// }
